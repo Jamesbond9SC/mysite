f9cb90ab27fab59ca6b6f7ca40c1ea30c032b572
Jamesbond9SC <Jamesbond9SC@gmail.com>
2022-12-17 16:12:12 +0530

    alpha_1

--- main.py
+++ main.py
@@ -4,9 +4,7 @@ from flask import Flask
 # Initialising the app
 app = Flask(__name__)
 
-# Main domain
-@app.route("/")
-
 # Defining the hello world test function
+@app.route("/")
 def hello_world():
-	return "Hello World..."
+	return "<p>Hello World...</p>"
