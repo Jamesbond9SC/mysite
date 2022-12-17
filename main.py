c2845d9becbba49e778481c81377767ff477e4e6
Jamesbond9SC <Jamesbond9SC@gmail.com>
2022-12-17 19:45:12 +0530

    alpha_2

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 # All imports
 from flask import Flask
+from flask import render_template
 
 # Initialising the app
 app = Flask(__name__)
 
 # Defining the hello world test function
 @app.route("/")
-def hello_world():
-	return "<p>Hello World...</p>"
+def main_page():
+	return render_template("main_page.html")
+
+# Testing on localhost
+#if __name__ == "__main__":
+#	app.run(host="0.0.0.0")
